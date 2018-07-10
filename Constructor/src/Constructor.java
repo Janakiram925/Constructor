c29f1c3f5dcd1219606bf76121c7eb37ec489e9f
Janakiram925 <40818610+Janakiram925@users.noreply.github.com>
2018-07-10 16:14:53 +0530

    first commit

---
+++ Constructor/src/Constructor.java
@@ -0,0 +1,29 @@
+
+class Constructor 
+{
+private	int rollno ;
+	String name;
+	Constructor(int rollno,String name)
+	{
+		this.rollno = rollno;
+		this.name = name;
+	}
+	public int m1()
+	{
+		return rollno;
+	}
+	public String m2()
+	{
+		return name;
+	}
+}
+class External
+{
+	public static void main(String[] args) 
+	{
+		Constructor c1 = new Constructor(4258,"janakiram");
+		//Constructor c2 = new Constructor();
+		System.out.println(c1.m1());
+		System.out.println(c1.m2());
+	}
+}
